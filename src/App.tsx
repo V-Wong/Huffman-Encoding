513129c299c1eda810918a009561572953499eab
Vincent Wong <vincent@vwong.dev>
2020-12-14 17:55:50 +1100

    Add Helmet and meta information

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 import { Container } from "react-bootstrap";
 
 import HuffmanPanel from "./components/HuffmanPanel";
@@ -7,11 +8,22 @@ import "bootswatch/dist/darkly/bootstrap.min.css";
 
 function App() {
   return (
-    <div style={{minHeight: "100vh", display: "flex", alignItems: "center"}}>
-      <Container fluid>
-        <HuffmanPanel />
-      </Container>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Huffman Encoding</title>
+        <meta name="description" content="Huffman Encoding tool for visualising the binary tree formed." />
+        <meta name="author" content="Vincent Wong" />
+        <meta name="keywords" content="Canvas, Huffman Encoding algorithm" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+
+      <div style={{ minHeight: "100vh", display: "flex", alignItems: "center" }}>
+        <Container fluid>
+          <HuffmanPanel />
+        </Container>
+      </div>
+    </>
   );
 }
 
